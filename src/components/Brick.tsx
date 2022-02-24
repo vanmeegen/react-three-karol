f6d89e055cde88d7c6727518904ac83901183b4c
vanmeegen <marco.vanmeegen@id-consult.com>
2022-02-24 16:42:17 +0100

    fix rendering of markers

--- src/components/Brick.tsx
+++ src/components/Brick.tsx
@@ -7,8 +7,8 @@ import { RoundedBox } from "@react-three/drei";
 function BrickInternal<T>(props: any | { texture: any; heightUnits: number }) {
   return (
     <RoundedBox
-      position={[props.position[0] + 0.5, props.position[1] + 0.25, props.position[2] + 0.5]}
-      scale={[1, 0.5, 1]}
+      position={[props.position[0] + 0.5, props.position[1] + 0.5*props.heightUnits, props.position[2] + 0.5]}
+      scale={[1, props.heightUnits, 1]}
       radius={0.15}
     >
       {[...Array(6)].map((_, index) => (
